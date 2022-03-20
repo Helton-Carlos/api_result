bba10bb4c5e761a92299e08ecd55b16d02a6271c
Helton <heltoncarlospa@hotmail.com>
2022-03-20 13:54:34 -0300

    mudanca login-post-parse

--- src/app.js
+++ src/app.js
@@ -6,9 +6,9 @@ require("dotenv").config({ path: "conf.env" });
 
 const bodyParser = require("body-parser");
 const routes = require("./routes");
-
+app.use(bodyParser.json())
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+//app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/api", routes);
 app.listen(process.env.PORT, () => {
